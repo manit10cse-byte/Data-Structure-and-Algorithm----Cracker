28d34e734a834ced9771302fca09140ab74c0bb1
Manit Kumar <manit10cse@gmail.com>
2026-06-08 22:59:25 +0530

    Implement vector reversal function
    Added a function to reverse the elements of a vector.

--- Vector.cpp
+++ Vector.cpp
@@ -21,4 +21,35 @@ int main(){
     cout<<"Enter the element to search : ";
     cin>>search;
     cout<<linearsearch(vec,search);
-    }    
+    } 
+
+//Write a function to reverse the vector:
+#include<iostream>
+#include<vector>
+using namespace std;
+void ReverseVec(vector<int> &vec){
+    int size=vec.size();
+    int start=0;
+    int end=size-1;
+    while(start<=end){
+        swap(vec[start],vec[end]);
+        start+=1;
+        end-=1;
+    }
+}
+int main(){
+    int size;
+    cout<<"Enter the size of array : ";
+    cin>>size;
+    vector<int> vec(size);
+    cout<<"Enter the elements : ";
+    for(int i=0;i<size;i++){
+        cin>>vec[i];
+    }
+    cout<<"Reverse of vector : ";
+    ReverseVec(vec);
+    for(int i=0;i<size;i++){
+        cout<<vec[i]<<" ";
+    }
+    cout<<endl;
+}
